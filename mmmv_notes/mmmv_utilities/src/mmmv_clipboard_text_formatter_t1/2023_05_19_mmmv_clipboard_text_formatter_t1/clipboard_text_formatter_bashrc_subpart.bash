1ebb285d105fb15c2438144e1fe62aace61b6824
Martin Vahi <martin.vahi@softf1.com>
2023-05-19 22:13:40 +0300

    mmmv_clipboard_text_formatter_t1

---
+++ mmmv_notes/mmmv_utilities/src/mmmv_clipboard_text_formatter_t1/2023_05_19_mmmv_clipboard_text_formatter_t1/clipboard_text_formatter_bashrc_subpart.bash
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+#==========================================================================
+# Initial author of this file: Martin.Vahi@softf1.com
+# This file is in public domain.
+# The following line is a spdx.org license label line:
+# SPDX-License-Identifier: 0BSD
+#
+# The code in this file is meant to be part of the $HOME/.bashrc .
+# It has been tested to work on 2023_05_xx Windows10 version of the
+# Windows Subsystem for Linux (WSL), on an onld openSUSE Linux.
+# If xclip is installed on BSD, then it MIGHT also work on BSD, but
+# the current version HAS NOT been tested on BSD.
+#
+# List of dependencies described partly in EBNF style: 
+#     mmmv_polish_ABC_2_A_C_B_exec_t1, bc, grep, cat, uuid, uname, wait,
+#     ruby, bash, par, (sed|gsed), nice, echo, printf, rm, tr, wc, 
+#     ( xclip | ( clip.exe,powershell.exe ) )
+# 
+#==========================================================================
+
+# Call example: mmmv_cat_par_sed_t1 ./x.txt
+alias mmmv_cat_par_sed_30_t1="S_CMD_GNU_SED=\"sed\" ; if [ \"\`uname -a | grep -i 'BSD' \`\" != '' ]; then S_CMD_GNU_SED=\"gsed\" ; fi ; wait ; mmmv_polish_ABC_2_A_C_B_exec_t1 'cat ' \" | \$S_CMD_GNU_SED -e 's/[[:blank:]]\+/ /g' | par 30 | \$S_CMD_GNU_SED -e  's/^/    /g'\" " 
+alias mmmv_cat_par_sed_40_t1="S_CMD_GNU_SED=\"sed\" ; if [ \"\`uname -a | grep -i 'BSD' \`\" != '' ]; then S_CMD_GNU_SED=\"gsed\" ; fi ; wait ; mmmv_polish_ABC_2_A_C_B_exec_t1 'cat ' \" | \$S_CMD_GNU_SED -e 's/[[:blank:]]\+/ /g' | par 40 | \$S_CMD_GNU_SED -e  's/^/    /g'\" " 
+alias mmmv_cat_par_sed_50_t1="S_CMD_GNU_SED=\"sed\" ; if [ \"\`uname -a | grep -i 'BSD' \`\" != '' ]; then S_CMD_GNU_SED=\"gsed\" ; fi ; wait ; mmmv_polish_ABC_2_A_C_B_exec_t1 'cat ' \" | \$S_CMD_GNU_SED -e 's/[[:blank:]]\+/ /g' | par 50 | \$S_CMD_GNU_SED -e  's/^/    /g'\" "
+alias mmmv_cat_par_sed_60_t1="S_CMD_GNU_SED=\"sed\" ; if [ \"\`uname -a | grep -i 'BSD' \`\" != '' ]; then S_CMD_GNU_SED=\"gsed\" ; fi ; wait ; mmmv_polish_ABC_2_A_C_B_exec_t1 'cat ' \" | \$S_CMD_GNU_SED -e 's/[[:blank:]]\+/ /g' | par 60 | \$S_CMD_GNU_SED -e  's/^/    /g'\" "
+alias mmmv_cat_par_sed_70_t1="S_CMD_GNU_SED=\"sed\" ; if [ \"\`uname -a | grep -i 'BSD' \`\" != '' ]; then S_CMD_GNU_SED=\"gsed\" ; fi ; wait ; mmmv_polish_ABC_2_A_C_B_exec_t1 'cat ' \" | \$S_CMD_GNU_SED -e 's/[[:blank:]]\+/ /g' | par 70 | \$S_CMD_GNU_SED -e  's/^/    /g'\" " 
+
+alias mmmv_clipboard_text_2_citation_WSL_et_44_t1="S_FP_TMP=\"/tmp/tmp_\`uuid | sed -e 's/-//g'\`.txt\" ; wait ; echo \"--------tsitaadi----algus-------------- \`nice -n 2 powershell.exe -c Get-Clipboard\` --------tsitaadi----lõpp--------------\" | par 40 | sed -e 's/^/    /g' >> \$S_FP_TMP ; wait ; cat \$S_FP_TMP | clip.exe  ; wait ; rm -f \$S_FP_TMP ;"
+
+alias mmmv_clipboard_text_2_citation_WSL_uk_44_t1="S_FP_TMP=\"/tmp/tmp_\`uuid | sed -e 's/-//g'\`.txt\" ; wait ; echo \"--------citation----start-------------- \`nice -n 2 powershell.exe -c Get-Clipboard\` --------citation----end---------------\" | par 40 | sed -e 's/^/    /g' >> \$S_FP_TMP ; wait ; cat \$S_FP_TMP | clip.exe  ; wait ; rm -f \$S_FP_TMP ;"
+
+# At least some version of the "par" text reformatter
+# seems to add one space to the end of some lines
+# and with words that are longer than the specified line width, the
+# "par" seems to cut them at one character shorter location than the specified
+# line with. The very last line also seem to be kept one character
+# shorter than the specified line with. This, a bit more hackish,
+# version of the clipboard content formatting alias, tries to
+# compensate those "par" flaws by manually formatting the 
+# citation bounding lines, the 1 line and the last line.
+# 
+# This "clean copy" of the alias has the structure of 
+#
+#     alias mmmv_clipboard_text_2_citation_WSL_et_54_t1="
+#         <some code> ; wait ;
+#         ...
+#         <some code> ; wait ;
+#         "
+#
+# and it is meant to be copied to a single line after editing.
+# Vim macros are the tool here.
+#
+# alias mmmv_clipboard_text_2_citation_et_54_t1="
+#     S_FP_TMP_RAMPARTITION_OR_HDD=\"/tmp\" ; wait ; 
+#     S_CMD_GNU_SED=\"sed\" ; if [ \"\`uname -a | grep -i 'BSD' \`\" != '' ]; then S_CMD_GNU_SED=\"gsed\" ; fi ; wait ;
+#     S_CMD_CLIPBOARD_2_FILE=\"xclip -out \" ; wait ;
+#     S_CMD_FILE_2_CLIPBOARD=\"xclip -selection c -in \" ; wait ;
+#     if [ \"\`uname -a | grep -i 'Microsoft' \`\" != '' ]; then echo '' > /dev/null ; wait ;
+#         S_CMD_CLIPBOARD_2_FILE=\"powershell.exe -c Get-Clipboard \" ; wait ;
+#         S_CMD_FILE_2_CLIPBOARD=\"clip.exe \" ; wait ;
+#     fi ; wait ;
+#     S_FN_SUFFIX=\"\`uuid | \$S_CMD_GNU_SED -e 's/-//g'\`.txt\" ; wait ; 
+#     S_FP_TMP_0=\"\$S_FP_TMP_RAMPARTITION_OR_HDD/tmp_0_\$S_FN_SUFFIX\" ; wait ; 
+#     S_FP_TMP_1=\"\$S_FP_TMP_RAMPARTITION_OR_HDD/tmp_1_\$S_FN_SUFFIX\" ; wait ; 
+#     nice -n 2 \$S_CMD_CLIPBOARD_2_FILE | \$S_CMD_GNU_SED -e 's/[\\r][\\n]/\\n/g' | \$S_CMD_GNU_SED -e 's/[\\r]/\\n/g' | \$S_CMD_GNU_SED -e 's/^[[:blank:]]\+//g' | \$S_CMD_GNU_SED -e 's/[[:blank:]]\+/ /g' > \$S_FP_TMP_0 ; wait ; 
+#     SI_LINECOUNT=\"\`wc -l \$S_FP_TMP_0 | \$S_CMD_GNU_SED -e 's/[[:blank:]].\+//g' | \$S_CMD_GNU_SED -e 's/\$/+1/' | bc | tr -d '\\n' \`\" ; wait ;
+#     SI_ITERATIONCOUNT=\"\$SI_LINECOUNT\" ; wait ;
+#     for ((i = 0; i < \$SI_ITERATIONCOUNT ; i++)) do echo '' > /dev/null ; wait ;
+#         cat \$S_FP_TMP_0 | \$S_CMD_GNU_SED -e  '1{/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' > \$S_FP_TMP_1 ; wait ;
+#         cat \$S_FP_TMP_1 | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' > \$S_FP_TMP_0 ; wait ;
+#     done ; wait ;
+#     echo \"    ------------tsitaadi----algus---------------------\" >  \$S_FP_TMP_1 ; wait ; 
+#     cat \$S_FP_TMP_0 | par 50 | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e 's/^/    /g' >> \$S_FP_TMP_1 ; wait ; 
+#     echo \"    ------------tsitaadi----lõpp----------------------\" >> \$S_FP_TMP_1 ; wait ;
+#     cat \$S_FP_TMP_1 | \$S_CMD_FILE_2_CLIPBOARD ; wait ; 
+#     rm -f \$S_FP_TMP_0 ; wait ;
+#     rm -f \$S_FP_TMP_1 ; wait ;
+#     "
+
+
+alias mmmv_clipboard_text_2_citation_et_54_t1=" S_FP_TMP_RAMPARTITION_OR_HDD=\"/tmp\" ; wait ; S_CMD_GNU_SED=\"sed\" ; if [ \"\`uname -a | grep -i 'BSD' \`\" != '' ]; then S_CMD_GNU_SED=\"gsed\" ; fi ; wait ; S_CMD_CLIPBOARD_2_FILE=\"xclip -out \" ; wait ; S_CMD_FILE_2_CLIPBOARD=\"xclip -selection c -in \" ; wait ; if [ \"\`uname -a | grep -i 'Microsoft' \`\" != '' ]; then echo '' > /dev/null ; wait ; S_CMD_CLIPBOARD_2_FILE=\"powershell.exe -c Get-Clipboard \" ; wait ; S_CMD_FILE_2_CLIPBOARD=\"clip.exe \" ; wait ; fi ; wait ; S_FN_SUFFIX=\"\`uuid | \$S_CMD_GNU_SED -e 's/-//g'\`.txt\" ; wait ; S_FP_TMP_0=\"\$S_FP_TMP_RAMPARTITION_OR_HDD/tmp_0_\$S_FN_SUFFIX\" ; wait ; S_FP_TMP_1=\"\$S_FP_TMP_RAMPARTITION_OR_HDD/tmp_1_\$S_FN_SUFFIX\" ; wait ; nice -n 2 \$S_CMD_CLIPBOARD_2_FILE | \$S_CMD_GNU_SED -e 's/[\\r][\\n]/\\n/g' | \$S_CMD_GNU_SED -e 's/[\\r]/\\n/g' | \$S_CMD_GNU_SED -e 's/^[[:blank:]]\+//g' | \$S_CMD_GNU_SED -e 's/[[:blank:]]\+/ /g' > \$S_FP_TMP_0 ; wait ; SI_LINECOUNT=\"\`wc -l \$S_FP_TMP_0 | \$S_CMD_GNU_SED -e 's/[[:blank:]].\+//g' | \$S_CMD_GNU_SED -e 's/\$/+1/' | bc | tr -d '\\n' \`\" ; wait ; SI_ITERATIONCOUNT=\"\$SI_LINECOUNT\" ; wait ; for ((i = 0; i < \$SI_ITERATIONCOUNT ; i++)) do echo '' > /dev/null ; wait ; cat \$S_FP_TMP_0 | \$S_CMD_GNU_SED -e  '1{/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' > \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_1 | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' > \$S_FP_TMP_0 ; wait ; done ; wait ; echo \"    ------------tsitaadi----algus---------------------\" >  \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_0 | par 50 | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e 's/^/    /g' >> \$S_FP_TMP_1 ; wait ; echo \"    ------------tsitaadi----lõpp----------------------\" >> \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_1 | \$S_CMD_FILE_2_CLIPBOARD ; wait ; rm -f \$S_FP_TMP_0 ; wait ; rm -f \$S_FP_TMP_1 ; wait ; "
+
+alias mmmv_clipboard_text_2_citation_uk_54_t1=" S_FP_TMP_RAMPARTITION_OR_HDD=\"/tmp\" ; wait ; S_CMD_GNU_SED=\"sed\" ; if [ \"\`uname -a | grep -i 'BSD' \`\" != '' ]; then S_CMD_GNU_SED=\"gsed\" ; fi ; wait ; S_CMD_CLIPBOARD_2_FILE=\"xclip -out \" ; wait ; S_CMD_FILE_2_CLIPBOARD=\"xclip -selection c -in \" ; wait ; if [ \"\`uname -a | grep -i 'Microsoft' \`\" != '' ]; then echo '' > /dev/null ; wait ; S_CMD_CLIPBOARD_2_FILE=\"powershell.exe -c Get-Clipboard \" ; wait ; S_CMD_FILE_2_CLIPBOARD=\"clip.exe \" ; wait ; fi ; wait ; S_FN_SUFFIX=\"\`uuid | \$S_CMD_GNU_SED -e 's/-//g'\`.txt\" ; wait ; S_FP_TMP_0=\"\$S_FP_TMP_RAMPARTITION_OR_HDD/tmp_0_\$S_FN_SUFFIX\" ; wait ; S_FP_TMP_1=\"\$S_FP_TMP_RAMPARTITION_OR_HDD/tmp_1_\$S_FN_SUFFIX\" ; wait ; nice -n 2 \$S_CMD_CLIPBOARD_2_FILE | \$S_CMD_GNU_SED -e 's/[\\r][\\n]/\\n/g' | \$S_CMD_GNU_SED -e 's/[\\r]/\\n/g' | \$S_CMD_GNU_SED -e 's/^[[:blank:]]\+//g' | \$S_CMD_GNU_SED -e 's/[[:blank:]]\+/ /g' > \$S_FP_TMP_0 ; wait ; SI_LINECOUNT=\"\`wc -l \$S_FP_TMP_0 | \$S_CMD_GNU_SED -e 's/[[:blank:]].\+//g' | \$S_CMD_GNU_SED -e 's/\$/+1/' | bc | tr -d '\\n' \`\" ; wait ; SI_ITERATIONCOUNT=\"\$SI_LINECOUNT\" ; wait ; for ((i = 0; i < \$SI_ITERATIONCOUNT ; i++)) do echo '' > /dev/null ; wait ; cat \$S_FP_TMP_0 | \$S_CMD_GNU_SED -e  '1{/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' > \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_1 | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' > \$S_FP_TMP_0 ; wait ; done ; wait ; echo \"    ------------citation----start---------------------\" >  \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_0 | par 50 | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e 's/^/    /g' >> \$S_FP_TMP_1 ; wait ; echo \"    ------------citation----end-----------------------\" >> \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_1 | \$S_CMD_FILE_2_CLIPBOARD ; wait ; rm -f \$S_FP_TMP_0 ; wait ; rm -f \$S_FP_TMP_1 ; wait ; "
+
+
+
+alias mmmv_clipboard_text_2_citation_et_64_t1=" S_FP_TMP_RAMPARTITION_OR_HDD=\"/tmp\" ; wait ; S_CMD_GNU_SED=\"sed\" ; if [ \"\`uname -a | grep -i 'BSD' \`\" != '' ]; then S_CMD_GNU_SED=\"gsed\" ; fi ; wait ; S_CMD_CLIPBOARD_2_FILE=\"xclip -out \" ; wait ; S_CMD_FILE_2_CLIPBOARD=\"xclip -selection c -in \" ; wait ; if [ \"\`uname -a | grep -i 'Microsoft' \`\" != '' ]; then echo '' > /dev/null ; wait ; S_CMD_CLIPBOARD_2_FILE=\"powershell.exe -c Get-Clipboard \" ; wait ; S_CMD_FILE_2_CLIPBOARD=\"clip.exe \" ; wait ; fi ; wait ; S_FN_SUFFIX=\"\`uuid | \$S_CMD_GNU_SED -e 's/-//g'\`.txt\" ; wait ; S_FP_TMP_0=\"\$S_FP_TMP_RAMPARTITION_OR_HDD/tmp_0_\$S_FN_SUFFIX\" ; wait ; S_FP_TMP_1=\"\$S_FP_TMP_RAMPARTITION_OR_HDD/tmp_1_\$S_FN_SUFFIX\" ; wait ; nice -n 2 \$S_CMD_CLIPBOARD_2_FILE | \$S_CMD_GNU_SED -e 's/[\\r][\\n]/\\n/g' | \$S_CMD_GNU_SED -e 's/[\\r]/\\n/g' | \$S_CMD_GNU_SED -e 's/^[[:blank:]]\+//g' | \$S_CMD_GNU_SED -e 's/[[:blank:]]\+/ /g' > \$S_FP_TMP_0 ; wait ; SI_LINECOUNT=\"\`wc -l \$S_FP_TMP_0 | \$S_CMD_GNU_SED -e 's/[[:blank:]].\+//g' | \$S_CMD_GNU_SED -e 's/\$/+1/' | bc | tr -d '\\n' \`\" ; wait ; SI_ITERATIONCOUNT=\"\$SI_LINECOUNT\" ; wait ; for ((i = 0; i < \$SI_ITERATIONCOUNT ; i++)) do echo '' > /dev/null ; wait ; cat \$S_FP_TMP_0 | \$S_CMD_GNU_SED -e  '1{/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' > \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_1 | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' > \$S_FP_TMP_0 ; wait ; done ; wait ; echo \"    ------------------tsitaadi----algus-------------------------\" >  \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_0 | par 60 | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e 's/^/    /g' >> \$S_FP_TMP_1 ; wait ; echo \"    ------------------tsitaadi----lõpp--------------------------\" >> \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_1 | \$S_CMD_FILE_2_CLIPBOARD ; wait ; rm -f \$S_FP_TMP_0 ; wait ; rm -f \$S_FP_TMP_1 ; wait ; "
+
+alias mmmv_clipboard_text_2_citation_uk_64_t1=" S_FP_TMP_RAMPARTITION_OR_HDD=\"/tmp\" ; wait ; S_CMD_GNU_SED=\"sed\" ; if [ \"\`uname -a | grep -i 'BSD' \`\" != '' ]; then S_CMD_GNU_SED=\"gsed\" ; fi ; wait ; S_CMD_CLIPBOARD_2_FILE=\"xclip -out \" ; wait ; S_CMD_FILE_2_CLIPBOARD=\"xclip -selection c -in \" ; wait ; if [ \"\`uname -a | grep -i 'Microsoft' \`\" != '' ]; then echo '' > /dev/null ; wait ; S_CMD_CLIPBOARD_2_FILE=\"powershell.exe -c Get-Clipboard \" ; wait ; S_CMD_FILE_2_CLIPBOARD=\"clip.exe \" ; wait ; fi ; wait ; S_FN_SUFFIX=\"\`uuid | \$S_CMD_GNU_SED -e 's/-//g'\`.txt\" ; wait ; S_FP_TMP_0=\"\$S_FP_TMP_RAMPARTITION_OR_HDD/tmp_0_\$S_FN_SUFFIX\" ; wait ; S_FP_TMP_1=\"\$S_FP_TMP_RAMPARTITION_OR_HDD/tmp_1_\$S_FN_SUFFIX\" ; wait ; nice -n 2 \$S_CMD_CLIPBOARD_2_FILE | \$S_CMD_GNU_SED -e 's/[\\r][\\n]/\\n/g' | \$S_CMD_GNU_SED -e 's/[\\r]/\\n/g' | \$S_CMD_GNU_SED -e 's/^[[:blank:]]\+//g' | \$S_CMD_GNU_SED -e 's/[[:blank:]]\+/ /g' > \$S_FP_TMP_0 ; wait ; SI_LINECOUNT=\"\`wc -l \$S_FP_TMP_0 | \$S_CMD_GNU_SED -e 's/[[:blank:]].\+//g' | \$S_CMD_GNU_SED -e 's/\$/+1/' | bc | tr -d '\\n' \`\" ; wait ; SI_ITERATIONCOUNT=\"\$SI_LINECOUNT\" ; wait ; for ((i = 0; i < \$SI_ITERATIONCOUNT ; i++)) do echo '' > /dev/null ; wait ; cat \$S_FP_TMP_0 | \$S_CMD_GNU_SED -e  '1{/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' > \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_1 | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' > \$S_FP_TMP_0 ; wait ; done ; wait ; echo \"    ------------------citation----start-------------------------\" >  \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_0 | par 60 | \$S_CMD_GNU_SED -e '\${/^[[:blank:]]*\$/d}' | \$S_CMD_GNU_SED -e 's/^/    /g' >> \$S_FP_TMP_1 ; wait ; echo \"    ------------------citation----end---------------------------\" >> \$S_FP_TMP_1 ; wait ; cat \$S_FP_TMP_1 | \$S_CMD_FILE_2_CLIPBOARD ; wait ; rm -f \$S_FP_TMP_0 ; wait ; rm -f \$S_FP_TMP_1 ; wait ; "
+
+#==========================================================================
+# Thank You for studying this script :-)
+#--------------------------------------------------------------------------
+# S_VERSION_OF_THIS_FILE="657b82a3-2f2b-4856-af1a-6153f03157e7"
+#========================================================================== 
